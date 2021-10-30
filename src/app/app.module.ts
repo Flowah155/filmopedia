7412cc9a5028a671c818f5a5c1d98ffb84e5da6e
Elena Medina <elenamedina155@gmail.com>
2021-10-29 21:06:32 -0500

    Add busqueda component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+import { BusquedaService } from './servicios/busqueda/busqueda.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [BusquedaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
