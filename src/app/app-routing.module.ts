62c2e78736e1155bcb8f6093e7512b8a22bfd205
Elena Medina <elenamedina155@gmail.com>
2021-10-29 22:10:13 -0500

    Integrate busqueda component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component'
 import { DetallesPeliculaComponent } from './componentes/detalles-pelicula/detalles-pelicula.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 
 const routes: Routes = [
+  { path: '', component: InicioComponent },
   { path: 'detalles', component: DetallesPeliculaComponent },
-  {path: '', component: InicioComponent}
+  { path: 'busqueda', component: BusquedaComponent}
 ];
 
 
